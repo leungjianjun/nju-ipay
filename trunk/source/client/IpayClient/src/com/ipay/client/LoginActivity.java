95b4768dee1c755461e7342de4bca34a7acf896d
yimingnju@gmail.com <yimingnju@gmail.com@5c6265d8-8a90-3e41-1436-53c5f2eaa411>
2011-07-22 12:43:39 +0000

    登录，初步

--- trunk/source/client/IpayClient/src/com/ipay/client/LoginActivity.java
+++ trunk/source/client/IpayClient/src/com/ipay/client/LoginActivity.java
@@ -4,11 +4,55 @@
 package com.ipay.client;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * @author tangym
  *
  */
 public class LoginActivity extends Activity {
+	
+	private static final String TAG="LoginActivity";
+	private static final String RUNNING_STATUS="running";
+	
+	private String username;
+	private String password;
+//	private User user;
+	
+	//views
+	private EditText usernameEdit;
+	private EditText passwordEdit;
+	private Button signinBtn;
+	private ProgressDialog progressDialog;
+	
+	private SharedPreferences preferences;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		Log.d(TAG, "onCreate");
+		super.onCreate(savedInstanceState);
+		
+        // No Title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        requestWindowFeature(Window.FEATURE_PROGRESS);
+        
+        //获取偏好设置
+        preferences=PreferenceManager.getDefaultSharedPreferences(this);
+        
+        setContentView(R.layout.login);
+        
+		
+	}
+	
+	
+	
 
 }
--- trunk/source/client/IpayClient/src/com/ipay/client/ui/base/BaseActivity.java
+++ trunk/source/client/IpayClient/src/com/ipay/client/ui/base/BaseActivity.java
@@ -4,11 +4,33 @@
 package com.ipay.client.ui.base;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
 
 /**
  * @author tangym
- *
+ * 
  */
 public class BaseActivity extends Activity {
+	private static final String TAG = "BaseActivity";
+	protected SharedPreferences preferences;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		_onCreate(savedInstanceState);
+	}
+
+	/**
+	 * 真实的onCreate方法
+	 * 
+	 * @param savedInstanceState
+	 * @return
+	 */
+	protected boolean _onCreate(Bundle savedInstanceState) {
+		return true;
+
+	}
 
 }
