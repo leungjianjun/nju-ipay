1271c08cb7050b2b047ba67360fa2fb00cda724e
yimingnju@gmail.com <yimingnju@gmail.com@5c6265d8-8a90-3e41-1436-53c5f2eaa411>
2011-07-25 13:05:44 +0000

    1. 添加顶部导航栏 navi_bar 2. 添加底部菜单栏 menu_bar 3. TODO：导航栏、菜单栏具体功能的图标制作；底部菜单栏还不能点击，下个版本修改。

--- trunk/source/client/IpayClient/src/com/ipay/client/ShoppingCartActivity.java
+++ trunk/source/client/IpayClient/src/com/ipay/client/ShoppingCartActivity.java
@@ -3,10 +3,24 @@
  */
 package com.ipay.client;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
 /**
  * @author tangym
  *
  */
-public class ShoppingCartActivity {
+public class ShoppingCartActivity  extends Activity{
+	private static final String TAG = "ShoppingCartActivity";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		requestWindowFeature(Window.FEATURE_PROGRESS);
+		setContentView(R.layout.main);
+	}
 
 }
