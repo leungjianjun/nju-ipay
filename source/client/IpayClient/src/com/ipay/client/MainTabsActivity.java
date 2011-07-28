35c688e874b62ec623c9a1536d5496a008c14e53
yimingnju@gmail.com <yimingnju@gmail.com@5c6265d8-8a90-3e41-1436-53c5f2eaa411>
2011-07-28 12:40:59 +0000

    1. 首页界面 2. 修正了tab的返回后无法选中的问题。

--- source/client/IpayClient/src/com/ipay/client/MainTabsActivity.java
+++ source/client/IpayClient/src/com/ipay/client/MainTabsActivity.java
@@ -7,66 +7,84 @@ import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Window;
+import android.view.View;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TabHost;
 
 /**
  * @author tangym
- *
+ * 
  */
 public class MainTabsActivity extends TabActivity {
-	
+	private static final String TAG="MainTabsActivity";
+
 	private TabHost tabhost;
+	private RadioGroup tabGroup;
+	private int selected;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		Log.d(TAG, "onCreate");
+		
 		setContentView(R.layout.main_tabs);
-		tabhost=getTabHost();
-		Intent tab1 = new Intent(this, TestActivity1.class);
-		tabhost.addTab(tabhost.newTabSpec("tab1").setIndicator("TAB1").setContent(tab1));
+		tabhost = getTabHost();
+		Intent home = new Intent(this, HomeActivity.class);
+		tabhost.addTab(tabhost.newTabSpec("HOME").setIndicator("HOME")
+				.setContent(home));
 		Intent tab2 = new Intent(this, TestActivity2.class);
-		tabhost.addTab(tabhost.newTabSpec("tab2").setIndicator("TAB2").setContent(tab2));
+		tabhost.addTab(tabhost.newTabSpec("tab2").setIndicator("TAB2")
+				.setContent(tab2));
 		Intent tab3 = new Intent(this, TestActivity3.class);
-		tabhost.addTab(tabhost.newTabSpec("tab3").setIndicator("TAB3").setContent(tab3));
+		tabhost.addTab(tabhost.newTabSpec("tab3").setIndicator("TAB3")
+				.setContent(tab3));
 		
+		Intent account = new Intent(this, TestActivity1.class);
+		tabhost.addTab(tabhost.newTabSpec("ACCOUNT").setIndicator("ACCOUNT")
+				.setContent(account));
+
+		tabGroup = (RadioGroup) this
+				.findViewById(R.id.tab_group);
 		
-		RadioGroup mainGroup=(RadioGroup)this.findViewById(R.id.test_menu_bar);
-		mainGroup.setOnCheckedChangeListener(new OnCheckedChangeListener()
-		{
- 	       @Override
-			public void onCheckedChanged(RadioGroup arg0, int rid) {
+		//绑定tabs和Radio Buttons
+		tabGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(RadioGroup group, int rid) {
 				// TODO Auto-generated method stub
-			 Log.d("radiou group", "you selected="+rid);
-			 switch(rid)
-			 {case R.id.radio_button0://Ê×Ò³
-				 tabhost.setCurrentTabByTag("tab1");
-				// msgTitle.setVisibility(View.GONE);
-				 break;
-			 case R.id.radio_button1://ÐÅÏ¢
-				 tabhost.setCurrentTabByTag("tab2");
-				// msgTitle.setVisibility(View.VISIBLE);
-				 break;
-			 case R.id.radio_button2://×ÊÁÏ
-				 tabhost.setCurrentTabByTag("tab3");
-				 //msgTitle.setVisibility(View.GONE);
-				 break;
-			 case R.id.radio_button3://ËÑË÷
-				 tabhost.setCurrentTabByTag("TAB_SEARCH");
-				 //msgTitle.setVisibility(View.GONE);
-				 break;
-			 case R.id.radio_button4://žü¶à	
-				 //msgTitle.setVisibility(View.GONE);
-				 tabhost.setCurrentTabByTag("TAB_MORE");
-				 
-			 }
+				Log.d("", "you selected=" + rid);
+				switch (rid) {
+				case R.id.radio_button0:
+					tabhost.setCurrentTabByTag("HOME");
+					selected=0;
+					
+					break;
+				case R.id.radio_button1:
+					tabhost.setCurrentTabByTag("tab2");
+					selected=1;
+					
+					break;
+				case R.id.radio_button2:
+					tabhost.setCurrentTabByTag("tab3");
+					selected=2;
+					break;
+				case R.id.radio_button3:
+					tabhost.setCurrentTabByTag("ACCOUNT");
+					selected=3;
+					break;
+				case R.id.radio_button4:
+					Log.d(TAG,"more checked");
+					
+					Intent i=new Intent(MainTabsActivity.this, TestActivity2.class);
+					startActivity(i);
+					group.check(selected);
+					break;
+
+				}
 			}
-			
-		}
-		);
+
+		});
 	}
 
 }
