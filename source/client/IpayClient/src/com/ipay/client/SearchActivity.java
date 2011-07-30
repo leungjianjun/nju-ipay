79e696c6865c50f2234fe986c2e16009c0984b7c
yimingnju@gmail.com <yimingnju@gmail.com@5c6265d8-8a90-3e41-1436-53c5f2eaa411>
2011-07-30 12:43:08 +0000

    search

---
+++ source/client/IpayClient/src/com/ipay/client/SearchActivity.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.ipay.client;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author tangym
+ *
+ */
+public class SearchActivity extends Activity {
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.search);
+	}
+
+}
