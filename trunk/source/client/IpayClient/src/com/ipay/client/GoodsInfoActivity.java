ccaaca2ed0c099d7ce1c3350524137237afe5e1f
yimingnju@gmail.com <yimingnju@gmail.com@5c6265d8-8a90-3e41-1436-53c5f2eaa411>
2011-08-18 12:29:13 +0000

    商品详情界面。 布局为：图片，介绍，加入购物车按钮。

--- trunk/source/client/IpayClient/src/com/ipay/client/GoodsInfoActivity.java
+++ trunk/source/client/IpayClient/src/com/ipay/client/GoodsInfoActivity.java
@@ -3,10 +3,21 @@
  */
 package com.ipay.client;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * @author tangym
  *
  */
-public class GoodsInfoActivity {
+public class GoodsInfoActivity  extends Activity{
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.goods_info);
+	}
 
 }
