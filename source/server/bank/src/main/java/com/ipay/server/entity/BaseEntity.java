5511df1b0ce65dff35f4694b2d986928f3274a48
leungjianjun@gmail.com <leungjianjun@gmail.com@5c6265d8-8a90-3e41-1436-53c5f2eaa411>
2011-08-22 07:14:20 +0000

    银行服务端数据库设计初步完成

--- source/server/bank/src/main/java/com/ipay/server/entity/BaseEntity.java
+++ source/server/bank/src/main/java/com/ipay/server/entity/BaseEntity.java
@@ -1,5 +1,61 @@
 package com.ipay.server.entity;
 
+import java.util.Date;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Version;
+import javax.validation.constraints.NotNull;
+
+@MappedSuperclass
 public class BaseEntity {
+	
+	/**
+	 * 实体id,hibernate自主负责
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	/**
+	 * 版本管理,有hibernate自主负责
+	 */
+	@Version
+	private Integer version;
+	
+	/**
+	 * 注册时间
+	 */
+	@Temporal(value = TemporalType.TIMESTAMP)
+	@NotNull
+	private Date createDate;
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
 
 }
--- source/server/bank/src/main/java/com/ipay/server/entity/User.java
+++ source/server/bank/src/main/java/com/ipay/server/entity/User.java
@@ -1,5 +1,139 @@
 package com.ipay.server.entity;
 
-public class User {
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class User extends BaseEntity{
+	
+	@NotNull
+	@Size(min = 2, max = 25)
+	@Column(nullable = false, unique = true)
+	private String account;
+	
+	@NotNull
+	@Size(min = 2, max = 25)
+	private String password;
+	
+	private String realname;
+	
+	private String address;
+	
+	private String idnumber;
+	
+	private String phonenum;
+	
+	private String remark;
+	
+	@Temporal(value = TemporalType.DATE)
+	@NotNull
+	private Date birthday;
+	
+	private boolean sex;
+	
+	private boolean enbale = true;
+	
+	@OneToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn
+	private CreditCard creditCard;
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRealname() {
+		return realname;
+	}
+
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getIdnumber() {
+		return idnumber;
+	}
+
+	public void setIdnumber(String idnumber) {
+		this.idnumber = idnumber;
+	}
+
+	public String getPhonenum() {
+		return phonenum;
+	}
+
+	public void setPhonenum(String phonenum) {
+		this.phonenum = phonenum;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public boolean isSex() {
+		return sex;
+	}
+
+	public void setSex(boolean sex) {
+		this.sex = sex;
+	}
+
+	public boolean isEnbale() {
+		return enbale;
+	}
+
+	public void setEnbale(boolean enbale) {
+		this.enbale = enbale;
+	}
+
+	public CreditCard getCreditCard() {
+		return creditCard;
+	}
+
+	public void setCreditCard(CreditCard creditCard) {
+		this.creditCard = creditCard;
+	}
+	
 
 }
