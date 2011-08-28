a604998f47bb7002049f363a6476825a1a343c4f
lutianming1005@gmail.com <lutianming1005@gmail.com@5c6265d8-8a90-3e41-1436-53c5f2eaa411>
2011-08-28 02:47:04 +0000

    修改异常处理

--- source/client/IpayClient/src/com/ipay/client/communication/CommunicationManager.java
+++ source/client/IpayClient/src/com/ipay/client/communication/CommunicationManager.java
@@ -10,12 +10,12 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Map.Entry;
 
-import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.HttpVersion;
 import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.ClientConnectionManager;
@@ -115,8 +115,9 @@ public class CommunicationManager {
 	 * @return	失败返回null
 	 * @throws IOException 
 	 * @throws ClientProtocolException 
+	 * @throws RequestException 
 	 */
-	public int login(String username, String password) throws ClientProtocolException, IOException {
+	public int login(String username, String password) throws HttpResponseException,IOException{
 	
 		JSONObject data = new JSONObject();
 		try {
@@ -139,22 +140,27 @@ public class CommunicationManager {
 			
 			Log.d(TAG, "********execute post");
 			statusCode = response.getStatusLine().getStatusCode();
+			result = getJsonResult(response);
 			if(statusCode == HttpStatus.SC_OK){
 				String statusLine=response.getStatusLine().toString();
 				Log.d(TAG, "********response.getStatusLine()=="+statusLine);
 				Log.d(TAG, "********response.getStatusLine().getStatusCode() == OK");
-				
-				result = getJsonResult(response);
+					
 				String s=result.toString();
 				Log.d(TAG, "********Status=="+s);
+				
 				status = result.getBoolean("status");
+			}else{
+				String error = result.getString("error");
+				throw new HttpResponseException(statusCode, error);
 			}
-			
-		} 
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		}
 		catch (JSONException e) {
 			Log.d(TAG, "********JSONException: " + e.toString());
 			e.printStackTrace();
-		}
+		} 
 		//登陆成功
 		if(status == true){			
 			Log.d(TAG,"********status == true");
@@ -172,19 +178,25 @@ public class CommunicationManager {
 	 * @return
 	 * @throws IOException 
 	 * @throws ClientProtocolException 
+	 * @throws RequestException 
 	 */
-	public boolean logout() throws ClientProtocolException, IOException{
+	public boolean logout() throws HttpResponseException, IOException{
 		HttpGet get = new HttpGet(LOGOUT_URL);
 		get.setHeader(HTTP.CONTENT_TYPE,"application/json");
 		boolean status = false;
 		try {
 			HttpResponse response = httpClient.execute(get);
+			JSONObject result = getJsonResult(response);
+			;
 			if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
-				String retSrc = EntityUtils.toString(response.getEntity());
-			JSONObject result = new JSONObject(retSrc);
-			status = result.getBoolean("status");
+				status = result.getBoolean("status");
+			}else{
+				String error = result.getString("error");
+				throw new HttpResponseException(response.getStatusLine().getStatusCode(), error);
 			}
-		} catch (JSONException e) {
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		}catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -201,20 +213,24 @@ public class CommunicationManager {
 	 * @throws IOException 
 	 * @throws ClientProtocolException 
 	 */
-	public UserInfo getUserInfo() throws ClientProtocolException, IOException{
+	public UserInfo getUserInfo() throws HttpResponseException, IOException{
 		HttpGet get = new HttpGet(USER_INFO_URL);
 		get.setHeader(HTTP.CONTENT_TYPE,"application/json");
 		try {
 			HttpResponse response = httpClient.execute(get);
-			if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
-				String retSrc = EntityUtils.toString(response.getEntity());
-				JSONObject result = new JSONObject(retSrc);
+			JSONObject result = getJsonResult(response);
+			if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){			
 				UserInfo info = new UserInfo();
 				info.setAccount(result.getString(UserInfo.ACCOUNT));
 				info.setRealname(result.getString(UserInfo.REAL_NAME));
 				info.setPhone(result.getString(UserInfo.PHONE_NUM));
 				return info;
+			}else{
+				String error = result.getString("error");
+				throw new HttpResponseException(response.getStatusLine().getStatusCode(), error);
 			}
+		}catch (ClientProtocolException e) {
+			e.printStackTrace();
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -226,9 +242,9 @@ public class CommunicationManager {
 	 * @param info
 	 * @return
 	 * @throws IOException 
-	 * @throws ClientProtocolException 
+	 * @throws HttpResponseException
 	 */
-	public boolean setUserInfo(UserInfo info) throws ClientProtocolException, IOException{
+	public boolean setUserInfo(UserInfo info) throws HttpResponseException, IOException{
 		JSONObject jInfo = new JSONObject();
 		boolean status = false;
 		try {
@@ -241,11 +257,16 @@ public class CommunicationManager {
 		
 		try {
 			HttpResponse response = doPost(SET_USER_INFO_URL, jInfo);
-			if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){			
-				JSONObject result = getJsonResult(response);
+			JSONObject result = getJsonResult(response);
+			if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){						
 				status = result.getBoolean("status");
+			}else{
+				String error = result.getString("error");
+				throw new HttpResponseException(response.getStatusLine().getStatusCode(), error);
 			}
-		} catch (JSONException e) {
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		}catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -257,8 +278,9 @@ public class CommunicationManager {
 	 * @param oldPassword
 	 * @param newPassword
 	 * @return
+	 * @throws IOException 
 	 */
-	public boolean setPassword(String oldPassword,String newPassword){
+	public boolean setPassword(String oldPassword,String newPassword) throws HttpResponseException,IOException {
 		JSONObject data = new JSONObject();
 		try {
 			data.put("oldPassword", oldPassword);
@@ -271,15 +293,14 @@ public class CommunicationManager {
 		boolean status = false;
 		try {
 			HttpResponse response = doPost(SET_PASSWORD_URL, data);
+			JSONObject result = getJsonResult(response);
 			if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
-				JSONObject result = getJsonResult(response);
 				status = result.getBoolean("status");
+			}else{
+				String error = result.getString("error");
+				throw new HttpResponseException(response.getStatusLine().getStatusCode(), error);
 			}
 		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
@@ -292,15 +313,16 @@ public class CommunicationManager {
 	 * @param name
 	 * @param pageNum 从1开始
 	 * @return
+	 * @throws HttpResponseException,IOException 
 	 */
-	public ArrayList<Market> searchMarket(String name, int pageNum){
+	public ArrayList<Market> searchMarket(String name, int pageNum) throws HttpResponseException,IOException{
 		HttpGet get = new HttpGet(SEARCH_MARKET_URL+"name="+name+"&page="+pageNum);
 		get.setHeader(HTTP.CONTENT_TYPE,"application/json");
 		ArrayList<Market> markets = new ArrayList<Market>();
 		try {
 			HttpResponse response = httpClient.execute(get);
-			if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
-				JSONObject result = getJsonResult(response);
+			JSONObject result = getJsonResult(response);
+			if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){				
 				JSONArray jMarkets = result.getJSONArray("markets");
 				for(int i = 0; i < jMarkets.length(); i++){
 					Market market = new Market();
@@ -309,12 +331,11 @@ public class CommunicationManager {
 					market.setName(jMarket.getString(MarketInfo.NAME));
 					market.setLocation(jMarket.getString(MarketInfo.LOCATION));
 				}
-				
+			}else{
+				String error = result.getString("error");
+				throw new HttpResponseException(response.getStatusLine().getStatusCode(), error);
 			}
 		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (JSONException e) {
@@ -332,8 +353,10 @@ public class CommunicationManager {
 	 * @param username
 	 * @param context
 	 * @return
+	 * @throws IOException 
+	 * @throws HttpResponseException 
 	 */
-	public byte[] getEncryptPrivateKey(Context context){
+	public byte[] getEncryptPrivateKey(Context context) throws HttpResponseException, IOException{
 		//查找本地key
 		byte[] key = new byte[656];
 		
@@ -369,8 +392,9 @@ public class CommunicationManager {
 	 * 从服务器获得key
 	 * @param username
 	 * @return
+	 * @throws IOException 
 	 */
-	private byte[] downloadEncryptPrivateKey(String username){
+	private byte[] downloadEncryptPrivateKey(String username) throws HttpResponseException, IOException{
 		HttpGet get = new HttpGet(GET_KEY_URL);
 		get.setHeader(HTTP.CONTENT_TYPE,"application/json");
 		byte[] key = new byte[656];
@@ -381,11 +405,10 @@ public class CommunicationManager {
 				inputStream.read(key);
 				inputStream.close();
 				return key;
+			}else{
+				throw new HttpResponseException(response.getStatusLine().getStatusCode(), "error");
 			}
 		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -395,14 +418,15 @@ public class CommunicationManager {
 	 * 获得指定商场的详细信息
 	 * @param MarketId
 	 * @return
+	 * @throws IOException 
 	 */
-	public MarketInfo getMarketInfo(int MarketId){
+	public MarketInfo getMarketInfo(int MarketId) throws HttpResponseException, IOException{
 		HttpGet get = new HttpGet(MARKET_INFO_URL+"mid="+marketId);
 		get.setHeader(HTTP.CONTENT_TYPE,"application/json");
 		try {
 			HttpResponse response = httpClient.execute(get);
-			if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
-				JSONObject result = getJsonResult(response);
+			JSONObject result = getJsonResult(response);
+			if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){				
 				Log.d(TAG,"获取商场信息："+result.toString());
 				MarketInfo marketInfo = new MarketInfo(marketId);
 				marketInfo.setName(result.getString(MarketInfo.NAME));
@@ -412,11 +436,11 @@ public class CommunicationManager {
 				marketInfo.setComplainPhone(result.getString(MarketInfo.COMPLAIN_PHONE));
 				marketInfo.setCreateDate(result.getString(MarketInfo.CREATE_DATE));
 				return marketInfo;
+			}else{
+				String error = result.getString("error");
+				throw new HttpResponseException(response.getStatusLine().getStatusCode(), error);
 			}
 		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (JSONException e) {
@@ -429,8 +453,10 @@ public class CommunicationManager {
 	/**
 	 * 获得当前接入的商场信息
 	 * @return
+	 * @throws IOException 
+	 * @throws HttpResponseException 
 	 */
-	public MarketInfo getMarketInfo(){
+	public MarketInfo getMarketInfo() throws HttpResponseException, IOException{
 		return getMarketInfo(marketId);
 	}
 	/**
@@ -440,20 +466,20 @@ public class CommunicationManager {
 	 * @throws ClientProtocolException
 	 * @throws IOException
 	 */
-	public ArrayList<SpecialProduct> getSpecialProducts(int pageNum) throws ClientProtocolException, IOException{
+	public ArrayList<SpecialProduct> getSpecialProducts(int pageNum) throws HttpResponseException, IOException{
 		ArrayList<SpecialProduct> specialProducts = new ArrayList<SpecialProduct>();
 		//id 要改回 marketId
 		HttpGet get = new HttpGet(SPECIAL_PRODUCT_URL+"mid="+marketId+"&page="+pageNum);
 		get.setHeader(HTTP.CONTENT_TYPE,"application/json");
-	
-		HttpResponse response = httpClient.execute(get);
-		if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
-			JSONObject result = getJsonResult(response);
-			
-			Log.d(TAG,"特殊商品:"+result.toString());
+		
+			HttpResponse response;
 			try {
-				JSONArray jProducts = result.getJSONArray("specialProducts");
+				response = httpClient.execute(get);
+				JSONObject result = getJsonResult(response);
+			if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){			
+				Log.d(TAG,"特殊商品:"+result.toString());
 				
+					JSONArray jProducts = result.getJSONArray("specialProducts");				
 				for(int i = 0; i < jProducts.length(); i++){
 					SpecialProduct product = new SpecialProduct();
 					JSONObject jProduct = jProducts.getJSONObject(i);
@@ -464,13 +490,20 @@ public class CommunicationManager {
 					product.setMinImgUrl(jProduct.getString(Product.MIN_IMG_URL));
 					product.setAdWords(jProduct.getString(SpecialProduct.AD_WORDS));
 					specialProducts.add(product);
-				}
-				
+				}		
+		}else{
+			String error = result.getString("error");
+			throw new HttpResponseException(response.getStatusLine().getStatusCode(), error);
+		}
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}			
-		}
+			}
+			
+		
 		return specialProducts;
 	}
 	/**
@@ -480,15 +513,15 @@ public class CommunicationManager {
 	 * @throws ClientProtocolException
 	 * @throws IOException
 	 */
-	public ArrayList<Product> getHotProducts(int pageNum) throws ClientProtocolException, IOException{
+	public ArrayList<Product> getHotProducts(int pageNum) throws HttpResponseException, IOException{
 		ArrayList<Product> hotProducts = new ArrayList<Product>();
 		HttpGet get = new HttpGet(HOT_PRODUCT_URL+"mid="+marketId+"&page="+pageNum);
 		get.setHeader(HTTP.CONTENT_TYPE,"application/json");
-	
-		HttpResponse response = httpClient.execute(get);
-		if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+		try{
+			HttpResponse response = httpClient.execute(get);
 			JSONObject result = getJsonResult(response);
-			try {
+			if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){	
+			
 				JSONArray jProducts = result.getJSONArray("hotProducts");
 				
 				for(int i = 0; i < jProducts.length(); i++){
@@ -500,12 +533,18 @@ public class CommunicationManager {
 					product.setMinImgUrl(jProduct.getString(Product.MIN_IMG_URL));
 					hotProducts.add(product);
 				}
-				
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}			
+						
+		}else{
+			String error = result.getString("error");
+			throw new HttpResponseException(response.getStatusLine().getStatusCode(), error);
 		}
+		}catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return hotProducts;
 		
 	}
@@ -513,15 +552,15 @@ public class CommunicationManager {
 	 * 通过条形码获得商品信息
 	 * @param barcode
 	 * @return 查找失败返回null
+	 * @throws IOException 
 	 */
-	public Product getProductInfo(String barcode) {
+	public Product getProductInfo(String barcode) throws HttpResponseException, IOException {
 		HttpGet get = new HttpGet(PRODUCT_INFO_BY_BARCODE_URL+"mid="+marketId+"&code="+barcode);
 		get.setHeader(HTTP.CONTENT_TYPE,"application/json");
 		try {
 			HttpResponse response = httpClient.execute(get);
+			JSONObject result = getJsonResult(response);
 			if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
-				String retSrc = EntityUtils.toString(response.getEntity());
-				JSONObject result = new JSONObject(retSrc);
 				Product product = new Product();
 				product.setId(result.getInt(Product.ID));
 				product.setName(result.getString(Product.NAME));
@@ -537,11 +576,11 @@ public class CommunicationManager {
 					product.putAttr(attr.getString("key"), attr.getString("value"));
 				}
 				return product;
+			}else{
+				String error = result.getString("error");
+				throw new HttpResponseException(response.getStatusLine().getStatusCode(), error);
 			}
 		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (JSONException e) {
@@ -554,15 +593,15 @@ public class CommunicationManager {
 	 * 通过id获得商品信息
 	 * @param id
 	 * @return
+	 * @throws HttpResponseException, IOException 
 	 */
-	public Product getProductInfo(int id){
+	public Product getProductInfo(int id) throws HttpResponseException, IOException{
 		HttpGet get = new HttpGet(PRODUCT_INFO_BY_ID_URL+"pid="+id);
 		get.setHeader(HTTP.CONTENT_TYPE,"application/json");
 		try {
 			HttpResponse response = httpClient.execute(get);
+			JSONObject result = getJsonResult(response);
 			if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
-				String retSrc = EntityUtils.toString(response.getEntity());
-				JSONObject result = new JSONObject(retSrc);
 				Product product = new Product();
 				product.setId(result.getInt(Product.ID));
 				product.setName(result.getString(Product.NAME));
@@ -578,11 +617,11 @@ public class CommunicationManager {
 					product.putAttr(attr.getString("key"), attr.getString("value"));
 				}
 				return product;
+			}else{
+				String error = result.getString("error");
+				throw new HttpResponseException(response.getStatusLine().getStatusCode(), error);
 			}
 		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (JSONException e) {
@@ -638,8 +677,9 @@ public class CommunicationManager {
 	 * @param payPassword 支付密码
 	 * @param key 证书,在调用此方法前需先调用getEncryptPrivateKey获得证书
 	 * @return 支付失败返回null
+	 * @throws IOException 
 	 */
-	public Order pay(Session session, String payPassword, byte[] key) {
+	public Order pay(Session session, String payPassword, byte[] key) throws HttpResponseException, IOException {
 		boolean success = false;
 		
 		//私钥解密
@@ -671,13 +711,17 @@ public class CommunicationManager {
 		//post
 		try {
 			HttpResponse response = doPost(PAY_URL, param);
+			JSONObject result = getJsonResult(response);
 			if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
 				
+			}else{
+				String error = result.getString("error");
+				throw new HttpResponseException(response.getStatusLine().getStatusCode(), error);
 			}
 		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
-		} catch (IOException e) {
+		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
