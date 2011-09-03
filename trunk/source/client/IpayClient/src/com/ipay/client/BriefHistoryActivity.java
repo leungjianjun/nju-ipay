1f66859d88e3b1d34930a01837f38fd81349f5a9
yimingnju@gmail.com <yimingnju@gmail.com@5c6265d8-8a90-3e41-1436-53c5f2eaa411>
2011-09-03 10:54:49 +0000

    资源整合

--- trunk/source/client/IpayClient/src/com/ipay/client/BriefHistoryActivity.java
+++ trunk/source/client/IpayClient/src/com/ipay/client/BriefHistoryActivity.java
@@ -4,17 +4,26 @@
 package com.ipay.client;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import com.ipay.client.ui.base.BaseListActivity;
+import com.ipay.client.ui.base.Pageable;
 import com.ipay.client.ui.component.NaviBarBack;
 
 /**
  * @author tangym
  *
  */
-public class BriefHistoryActivity extends BaseListActivity {
+public class BriefHistoryActivity extends BaseListActivity implements Pageable {
 	
+	private int page=1;
+	private View listHeader;
+	private View listFooter;
+	ProgressBar progressBar;
 	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -27,7 +36,16 @@ public class BriefHistoryActivity extends BaseListActivity {
 		naviBar=new NaviBarBack(this);
 		naviBar.setTitle(R.string.brief_history_title);
 		
+		listView=(ListView)findViewById(R.id.brief_record_list);
+		listHeader = View.inflate(this, R.layout.listview_header, null);
+		listFooter = View.inflate(this, R.layout.listview_footer, null);
+		listView.addHeaderView(listHeader, null, true);
+		listView.addFooterView(listFooter, null, true);
+		progressBar = (ProgressBar) listFooter
+				.findViewById(R.id.list_footer_progress_bar);
+
 	}
+	
 
 	
 	@Override
@@ -48,6 +66,20 @@ public class BriefHistoryActivity extends BaseListActivity {
 	protected void bindItemOnClickListener() {
 		
 
+	}
+
+
+	@Override
+	public void nextPage() {
+		page++;
+		doRetrive();
+		
+	}
+
+
+	@Override
+	public int getCurrentPage() {
+		return page;
 	}
 
 }
